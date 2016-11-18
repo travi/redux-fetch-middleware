6100632237955575821ab41d84a14207cd6e96e3
Matt Travi <programmer@travi.org>
2016-11-18 14:09:35 -0600

    build(rollup): configured rollup to create es5 and es6 bundles

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+    entry: 'src/middleware.js',
+    plugins: [
+        babel({
+            babelrc: false,
+            exclude: ['./node_modules/**'],
+            presets: ['es2015-rollup', 'stage-2']
+        })
+    ],
+    targets: [
+        {dest: 'lib/middleware.cjs.js', format: 'cjs'},
+        {dest: 'lib/middleware.es.js', format: 'es'}
+    ]
+};
