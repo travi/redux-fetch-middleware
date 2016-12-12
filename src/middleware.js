f00d7d0956bc001167126e9cac017526252874ee
Matt Travi <matt@gaincompliance.com>
2016-12-12 15:52:32 -0600

    fix(default-export): adjusted the default export to enable the presence of the default function in t

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,22 @@
 import {use} from '@travi/ioc';
 
-export default (session = {}) => ({dispatch}) => (next) => (action) => {
-    const
-        fetcher = use('fetcher-factory')(session),
-        {fetch, initiate, success, failure, data} = action;
+export default function (session = {}) {
+    return ({dispatch}) => (next) => (action) => {
+        const
+            fetcher = use('fetcher-factory')(session),
+            {fetch, initiate, success, failure, data} = action;
 
-    if (!fetch) {
-        return next(action);
-    }
+        if (!fetch) {
+            return next(action);
+        }
 
-    dispatch({type: initiate, ...data});
+        dispatch({type: initiate, ...data});
 
-    return fetch(fetcher)
-        .then((response) => dispatch({type: success, resource: response}))
-        .catch((err) => {
-            dispatch({type: failure, error: err});
-            return Promise.reject(err);
-        });
-};
+        return fetch(fetcher)
+            .then((response) => dispatch({type: success, resource: response}))
+            .catch((err) => {
+                dispatch({type: failure, error: err});
+                return Promise.reject(err);
+            });
+    };
+}
