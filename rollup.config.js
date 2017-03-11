8da99451b070058678a705f755b51449ac245ba0
Matt Travi <programmer@travi.org>
2017-03-11 01:31:08 -0600

    refactor(eslint-config): aligned code to the eslint-config update

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: 'src/middleware.js',
-    plugins: [
-        babel({
-            babelrc: false,
-            exclude: ['./node_modules/**'],
-            presets: ['es2015-rollup', 'stage-2']
-        })
-    ],
-    targets: [
-        {dest: 'lib/middleware.cjs.js', format: 'cjs', exports: 'named'},
-        {dest: 'lib/middleware.es.js', format: 'es'}
-    ]
+  entry: 'src/middleware.js',
+  plugins: [
+    babel({
+      babelrc: false,
+      exclude: ['./node_modules/**'],
+      presets: ['es2015-rollup', 'stage-2']
+    })
+  ],
+  targets: [
+    {dest: 'lib/middleware.cjs.js', format: 'cjs', exports: 'named'},
+    {dest: 'lib/middleware.es.js', format: 'es'}
+  ]
 };
